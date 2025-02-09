41e8365be9272c3638b896c4d99ebbcd1a31f128
Jawad <jawadnasiri2222@gmail.com>
2025-02-09 20:03:08 +0100

    add the about page

---
+++ view/about.html.php
@@ -0,0 +1,177 @@
+<?php include "../public/header.html.php" ?>
+
+<div class="about">
+    <div class="about-banner"></div>
+
+    <div class="collection-container">
+        <div class="box1">
+            <img src="/uni-watch/public/assets/images/about-page/about-01.jpg" alt="watch">
+        </div>
+
+        <div class="box2">
+            <img src="/uni-watch/public/assets/images/about-page/about-02.jpg" alt="watch">
+        </div>
+
+        <div class="box3">
+            <img src="/uni-watch/public/assets/images/about-page/about-03.jpg" alt="watch">
+        </div>
+    </div>
+
+    <div class="service-container">
+
+        <div class="service">
+
+            <div class="img-box">
+                <img src="/uni-watch/public/assets/images/about-page/icon/01.png" alt="icon">
+            </div>
+
+            <div class="content-box">
+                <p class="service-title">Digital Marketing</p>
+                <p class="content">Lorem Khaled Ipsum is a major key to success. Eliptical talk. The key to more success is to get a massage once a week, very important, major key, cloth talk.</p>
+            </div>
+
+        </div>
+
+        <div class="service">
+
+            <div class="img-box">
+                <img src="/uni-watch/public/assets/images/about-page/icon/02.png" alt="icon">
+            </div>
+
+            <div class="content-box">
+                <p class="service-title">Digital Marketing</p>
+                <p class="content">Lorem Khaled Ipsum is a major key to success. Eliptical talk. The key to more success is to get a massage once a week, very important, major key, cloth talk.</p>
+            </div>
+            
+        </div>
+
+        <div class="service">
+
+            <div class="img-box">
+                <img src="/uni-watch/public/assets/images/about-page/icon/03.png" alt="icon">
+            </div>
+
+            <div class="content-box">
+                <p class="service-title">Digital Marketing</p>
+                <p class="content">Lorem Khaled Ipsum is a major key to success. Eliptical talk. The key to more success is to get a massage once a week, very important, major key, cloth talk.</p>
+            </div>
+            
+        </div>
+
+        <div class="service">
+
+            <div class="img-box">
+                <img src="/uni-watch/public/assets/images/about-page/icon/04.png" alt="icon">
+            </div>
+
+            <div class="content-box">
+                <p class="service-title">Digital Marketing</p>
+                <p class="content">Lorem Khaled Ipsum is a major key to success. Eliptical talk. The key to more success is to get a massage once a week, very important, major key, cloth talk.</p>
+            </div>
+            
+        </div>
+
+        <div class="service">
+
+            <div class="img-box">
+                <img src="/uni-watch/public/assets/images/about-page/icon/05.png" alt="icon">
+            </div>
+
+            <div class="content-box">
+                <p class="service-title">Digital Marketing</p>
+                <p class="content">Lorem Khaled Ipsum is a major key to success. Eliptical talk. The key to more success is to get a massage once a week, very important, major key, cloth talk.</p>
+            </div>
+            
+        </div>
+
+        <div class="service">
+
+            <div class="img-box">
+                <img src="/uni-watch/public/assets/images/about-page/icon/06.png" alt="icon">
+            </div>
+
+            <div class="content-box">
+                <p class="service-title">Digital Marketing</p>
+                <p class="content">Lorem Khaled Ipsum is a major key to success. Eliptical talk. The key to more success is to get a massage once a week, very important, major key, cloth talk.</p>
+            </div>
+
+        </div>
+        
+
+    </div>
+
+    <div class="about-info-box">
+        <div class="info">
+            <div class="info-content">
+                <h2>make your time best</h2>
+                <p>Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna.</p>
+            </div>
+
+            <div class="btn">
+                <p>Shop Now</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="description-container">
+        <div class="description">
+            <div class="text-content">
+                <h1>Ultimate Designs</h1>
+                <p>Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam. <br><br>Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus. <br><br>In antiquity, a paragraph often was a single thought—and often a single sentence, usually a very long one. Writers today, however, tend not to go on the way classical authors did. In academic writing, most paragraphs include at least three sentences, though rarely more than ten</p>
+            </div>
+
+            <div class="img-desc-box">
+                <img src="/uni-watch/public/assets/images/about-page/a-01.jpg" alt="">
+            </div>
+        </div>
+
+        <div class="our-team">
+            <div class="team-title">
+                <span>Our Creative Workers</span>
+                <h1>Our Team</h1>
+            </div>
+
+            <div class="team">
+                <div class="member-box">
+                    <img src="/uni-watch/public/assets/images/about-page/member/team-1.jpg" alt="">
+
+                    <h4>Mr. Mike Banding</h4>
+                    <p>Manger</p>
+                </div>
+
+                <div class="member-box">
+                    <img src="/uni-watch/public/assets/images/about-page/member/team-2.jpg" alt="">
+
+                    <h4>Mr. Mike Banding</h4>
+                    <p>Manger</p>
+                </div>
+
+                <div class="member-box">
+                    <img src="/uni-watch/public/assets/images/about-page/member/team-3.jpg" alt="">
+
+                    <h4>Mr. Mike Banding</h4>
+                    <p>Manger</p>
+                </div>
+
+                <div class="member-box">
+                    <img src="/uni-watch/public/assets/images/about-page/member/team-4.jpg" alt="">
+
+                    <h4>Mr. Mike Banding</h4>
+                    <p>Manger</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+<?php include "../public/footer.html.php" ?>
